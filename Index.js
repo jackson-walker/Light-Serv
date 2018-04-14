ac0e2c92894e727df31e91b6a9799358b310aec0
Jackson Walker <cheezcakep@gmail.com>
2018-04-14 12:36:22 -0500

    Finished insital datagram

--- Index.js
+++ Index.js
@@ -2,9 +2,12 @@ var PORT = 6969;
 var HOST = '127.0.0.1';
 
 var datagram = require('dgram');
-var server = dgram.createSocket('udp4');
+var server = datagram.createSocket('udp4');
 
 server.on('listening', function () {
     var address = server.address();
-    console.log('UDP Server listening on ' + address.address + "+" address.port);
+    console.log('UDP Server listening on ' + address.address + ":" + address.port);
+});
+server.on('message', function (message, remote){
+    console.log(remote.address + ':' + remote.port +' - ' + message);
 });
